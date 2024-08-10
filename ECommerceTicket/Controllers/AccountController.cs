472bdae089d5d9a39c53f08d262f7e6991d3ce5f
Dominic James <dominic@bepeerless.co>
2024-08-10 01:30:29 +0100

    Add project files.

---
+++ ECommerceTicket/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceTicket.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Users()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+    }
+}
---
+++ ECommerceTicket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommerceTicket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
